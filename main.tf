9417e13d0be4ee251a9fd0447a11e0cfce0924e3
SRINIVAS <srinivaskadudhuram98@gmail.com>
2026-01-02 09:48:57 +0530

    added-1

--- main.tf
+++ main.tf
@@ -14,3 +14,20 @@ resource "aws_vpc_security_group_ingress_rule" "myingressrules" {
   ip_protocol = "tcp"
   cidr_ipv4 = "0.0.0.0/0"
 }
+resource "aws_key_pair" "myownkey" {
+  public_key = file("~/.ssh/id_ed25519.pub")
+  key_name = "myshownkey"
+  
+}
+resource "aws_instance" "myec2" {
+  ami = "ami-02b8269d5e85954ef"
+  instance_type = "t3.micro"
+  key_name = aws_key_pair.myownkey.key_name
+  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.myownsg.id]
+  tags = {
+     Name = "myterraformec2"
+  }
+  
+  
+}
